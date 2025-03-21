2016576f98a5d040dd505b77d2eaca70124fa215
krythz43 <krithickrockz@gmail.com>
2025-03-21 17:46:44 +0530

    iExpense project

---
+++ expenseCalc/expenseCalc/ContentView.swift
@@ -0,0 +1,116 @@
+//
+//  ContentView.swift
+//  expenseCalc
+//
+//  Created by Nagaraj G on 21/03/25.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("Expense Calc")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+        }
+        .onAppear {
+            let expense = ExpenseItem(name: "Name", type: "Type", amount: 200)
+            expenses.items.append(expense)
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct AddView: View {
+    var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    @Environment(\.dismiss) var dismiss
+    
+    let types = ["Business", "Personal"]
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount",
+                          value: $amount,
+                          format: .currency(code: "USD"))
+                .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
---
+++ expenseCalc/expenseCalc/expenseCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseCalcApp.swift
+//  expenseCalc
+//
+//  Created by Nagaraj G on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct expenseCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
